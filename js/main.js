c40269ab06de4e82e23bd1758ffcf01048a9794b
youahleum <dkfma6033@naver.com>
2022-05-08 01:33:36 +0900

    json 파일 적용

--- js/main.js
+++ js/main.js
@@ -4,11 +4,12 @@ let year = today.getFullYear();
 let month = ("0" + (today.getMonth() + 1)).slice(-2);
 let day = ("0" + today.getDate()).slice(-2);
 let dateString = year + "-" + month + "-" + day;
-console.log(dateString);
 
-const LiElem = document.getElementsByClassName("recent_list li");
-const recentBox = document.getElementsByClassName("recent_box");
+let numComma = toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 
+const LiElem = document.querySelector(".recent_list li");
+const recentBox = document.getElementsByClassName("recent_box")[0];
+console.log(recentBox);
 // create Element
 const elDiv = document.createElement("div");
 const elLi = document.createElement("li");
@@ -41,31 +42,69 @@ async function recentCreateList() {
     return x.date == dateString;
   });
   console.log(today);
-  recentBox.appendChild(recent);
-  recent.appendChild(recentDate);
-  for (i = 0; i < obj.data.length; i++) {
-    // console.log(obj.data[i]);
-    let priceSum = 0;
-    if (obj.data[i].date === dateString) {
-      // let date = (elP.textContent = "오늘");
-      // recent.appendChild(date);
-      let history = obj.data[i].history;
-      let price = obj.data[i].price;
+  // recentBox.appendChild(recent);
+  // recent.appendChild(recentDate);
+  // for (i = 0; i < obj.data.length; i++) {
+  //   // console.log(obj.data[i]);
+  //   let priceSum = 0;
+  //   if (obj.data[i].date === dateString) {
+  //     // let date = (elP.textContent = "오늘");
+  //     // recent.appendChild(date);
 
-      // let
-    }
-  }
+  //     let history = obj.data[i].history;
+  //     let price = obj.data[i].price;
+  //     let recentDateEl = `<p>${history}</p><p>${price}</p>`;
+  //     let recentListEl = `<p>${history}</p><p>${price}</p>`;
+  //
+  //     // let
+  //   }
+  // }
 
   const groupValues = obj.data.reduce((acc, current) => {
     acc[current.date] = acc[current.date] || [];
-    acc[current.date].push(current.price);
+    acc[current.date].push(current);
     return acc;
   }, {});
   // 위에서 만든 객체를 key로 돌려서 새로운 객체 return
   const groups = Object.keys(groupValues).map((key) => {
-    return { date: key, price: groupValues[key] };
+    return { date: key, value: groupValues[key] };
   });
   console.log(groups);
+
+  groups.reverse().map((day) => {
+    let dateEl = day.date;
+    let priceSum = 0;
+    let recentListEl = ``;
+    for (i of day.value) {
+      // console.log(i.classify);
+      let history = i.history;
+      let price = i.price;
+      let incomePrice;
+
+      priceSum += price;
+      let commaPrice = price
+        .toString()
+        .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+      i.income === "out"
+        ? (incomePrice = `<p>${commaPrice}</p>`)
+        : (incomePrice = `<p style="color: #FF5F00;">+${commaPrice}</p>`);
+      recentListEl = recentListEl + `<li><p>${history}</p>${incomePrice}</li>`;
+    }
+    let commaPriceSum = priceSum
+      .toString()
+      .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+    let recentDateEl = `<p>${dateEl}</p><p>${commaPriceSum}원 지출</p>`;
+    let recentEl = new DOMParser().parseFromString(
+      `<div class="recent">
+      <div class="recent_date">${recentDateEl}</div>
+      <ul class="recent_list">
+        ${recentListEl}
+      </ul>
+    </div>`,
+      "text/html"
+    ).body.firstElementChild;
+    recentBox.appendChild(recentEl);
+  });
 }
 recentCreateList();
 // function recentCreateList() {
@@ -82,13 +121,8 @@ recentCreateList();
     <div class="recent_date">
       <p>오늘</p>
       <p>127,600원 지출</p>
-    </div>
-    <ul class="recent_list">
-      <li>
+    
         <p>미스터피자</p>
         <p>32,000</p>
-      </li>
-    </ul>
-  </div>
-</div>; */
+      ; */
 }

fdf518762f97591290639a2e0c2cd2973db03ded
youahleum <dkfma6033@naver.com>
2022-05-10 05:37:03 +0900

    transfer js 적용중

--- js/transfer.js
+++ js/transfer.js
@@ -2,23 +2,28 @@ const article1 = document.querySelector(".article1");
 const article2 = document.querySelector(".article2");
 const article3 = document.querySelector(".article3");
 const numInput = document.querySelector(".numInput");
+const accountName = document.querySelector(".article2_account_user_name");
+const accountNum = document.querySelector(".article2_account_user_name");
 // const nameValue = document.querySelector("account_user_name").innerHTML;
 // console.log(nameValue);
-console.log(numInput);
+console.log(article1);
+console.log(article2);
+console.log(article3);
+
 //
 
 let numStr = "";
 function numAdd(n) {
   numStr = numStr + n;
   console.log(numStr);
-
   numStr.startsWith("0") ? (numStr = numStr.slice(1)) : null;
   numStr.startsWith("0") ? (numStr = numStr.slice(1)) : null;
   numInput.innerText = numStr;
   console.log(numStr);
 }
-numInput.innerText = numStr;
-console.log(numStr);
+// numInput.innerText = numStr;
+// console.log(numStr);
+
 function del() {
   let a = numStr.length - 1;
   // console.log(a);
@@ -26,6 +31,13 @@ function del() {
   numInput.innerText = numStr;
   console.log(numStr);
 }
-function next() {
+
+function next1(name, accountNumber) {
+  article1.style.display = "none";
+  article2.style.display = "block";
+}
+
+function next2() {
   article2.style.display = "none";
+  article3.style.display = "block";
 }

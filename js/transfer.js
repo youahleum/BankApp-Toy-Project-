2c3e93ff31a604650fc044d3db0f504e5d17107b
youahleum <dkfma6033@naver.com>
2022-05-10 15:31:44 +0900

    transfer3 article 완성

--- js/transfer.js
+++ js/transfer.js
@@ -2,15 +2,20 @@ const article1 = document.querySelector(".article1");
 const article2 = document.querySelector(".article2");
 const article3 = document.querySelector(".article3");
 const numInput = document.querySelector(".numInput");
+// const art3accountName = document.querySelector(".article2_account_user_name");
+// const art3accountNum = document.querySelector(".article2_account_user_name");
+
+// article2
+let accountImgSrc = document.querySelector(".accountImg").src;
+console.log(accountImgSrc);
 const accountName = document.querySelector(".article2_account_user_name");
-const accountNum = document.querySelector(".article2_account_user_name");
+const accountNum = document.querySelector(".article2_account_number");
+
+// article3
+const transferName = document.querySelector(".art3_name");
+const transferAmount = document.querySelector(".art3_amount");
 // const nameValue = document.querySelector("account_user_name").innerHTML;
 // console.log(nameValue);
-console.log(article1);
-console.log(article2);
-console.log(article3);
-
-//
 
 let numStr = "";
 function numAdd(n) {
@@ -21,8 +26,6 @@ function numAdd(n) {
   numInput.innerText = numStr;
   console.log(numStr);
 }
-// numInput.innerText = numStr;
-// console.log(numStr);
 
 function del() {
   let a = numStr.length - 1;
@@ -32,7 +35,12 @@ function del() {
   console.log(numStr);
 }
 
-function next1(name, accountNumber) {
+function next1(picture, name, accountNumber) {
+  accountImgSrc = picture;
+  console.log(picture);
+  console.log(accountImgSrc);
+  accountName.textContent = name;
+  accountNum.textContent = accountNumber;
   article1.style.display = "none";
   article2.style.display = "block";
 }
@@ -41,3 +49,5 @@ function next2() {
   article2.style.display = "none";
   article3.style.display = "block";
 }
+
+function getName() {}
